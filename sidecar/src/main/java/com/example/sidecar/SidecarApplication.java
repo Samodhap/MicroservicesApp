2206dbadd10b6e8ccf8b4ccce74c2fdf00f41e49
Pallewatta Kankanamge Samodha Kanchani Pallewatta <ppallewatta@student.unimelb.edu.au>
2021-04-13 13:43:20 +1000

    Made changes required for docker-compose based deplyment

--- sidecar/src/main/java/com/example/sidecar/SidecarApplication.java
+++ sidecar/src/main/java/com/example/sidecar/SidecarApplication.java
@@ -2,14 +2,14 @@ package com.example.sidecar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.sidecar.EnableSidecar;
 
 @SpringBootApplication
 @EnableSidecar
+@EnableDiscoveryClient
 public class SidecarApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SidecarApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SidecarApplication.class, args);
+    }
 }
